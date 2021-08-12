fa65a990b9c7960053c99df0f32a6200ad70f2d2
armyants531 <giantmathbox@gmail.com>
2021-08-12 17:32:36 +0900

    third_commit

--- app/src/main/java/org/techtown/oasis/Fragment3.java
+++ app/src/main/java/org/techtown/oasis/Fragment3.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -45,10 +48,35 @@ public class Fragment3 extends Fragment {
     }
 
     private void initUI(ViewGroup rootView) {
+        EditText editText1;
         Spinner spinner1, spinner2;
-        String[] items = {};
-
+        String[] items1 = {"40kg 미만", "40~50kg", "50~60kg", "70~80kg", "80~90kg", "90~100kg", "100kg 초과"};
+        String[] items2 = {"140cm 미만", "140~150cm", "150~160cm", "160~170cm", "170~180cm", "180~190cm", "190cm 초과"};
         spinner1 = rootView.findViewById(R.id.spinner1);
         spinner2 = rootView.findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items1);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
