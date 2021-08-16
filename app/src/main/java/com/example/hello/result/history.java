e1190a84c91d66363f47442b4f82766258c9ed0e
gi-myewon <khw4473@gmail.com>
2021-08-16 20:43:38 +0900

    four commit

---
+++ app/src/main/java/com/example/hello/result/history.java
@@ -0,0 +1,16 @@
+package com.example.hello.result;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hello.R;
+
+public class history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
