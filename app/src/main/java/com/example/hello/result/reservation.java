ec1ed9ef765fb1bf3da0c5ae67a4e8d9d0f6802a
gi-myewon <khw4473@gmail.com>
2021-08-17 14:49:30 +0900

    TWELVE COMMIT

--- app/src/main/java/com/example/hello/result/reservation.java
+++ app/src/main/java/com/example/hello/result/reservation.java
@@ -36,9 +36,7 @@ public class reservation extends AppCompatActivity {
         setContentView(R.layout.activity_reservation);
         date_in=findViewById(R.id.date_input);
         time_in=findViewById(R.id.time_input);
-        date_time_in=findViewById(R.id.date_time_input);
 
-        date_time_in.setInputType(InputType.TYPE_NULL);
         date_in.setInputType(InputType.TYPE_NULL);
         time_in.setInputType(InputType.TYPE_NULL);
 
@@ -57,44 +55,7 @@ public class reservation extends AppCompatActivity {
             }
         });
 
-        date_time_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDateTimeDialog(date_time_in);
-            }
-        });
-    }
 
-    private void showDateTimeDialog(EditText date_time_in) {
-        Calendar calendar = Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-
-                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                        calendar.set(Calendar.MINUTE,minute);
-
-                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
-
-                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
-
-                    }
-                };
-                new TimePickerDialog(reservation.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
-                        calendar.get(Calendar.MINUTE),false).show();
-
-            }
-        };
-
-        new DatePickerDialog(reservation.this, dateSetListener,calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
     private void showTimeDialog(EditText time_in) {
