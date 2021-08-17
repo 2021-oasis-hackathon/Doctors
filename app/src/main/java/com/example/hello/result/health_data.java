5e5e15bbec293b36c22351ed76fb76378bb0553b
gi-myewon <khw4473@gmail.com>
2021-08-18 08:06:59 +0900

    sixteen commit

---
+++ app/src/main/java/com/example/hello/result/health_data.java
@@ -0,0 +1,16 @@
+package com.example.hello.result;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hello.R;
+
+
+public class health_data extends Fragment {
+
+}
