ed2674c3794bbb9672ef2a2e7c03fa94f9f8c8c8
armyants531 <giantmathbox@gmail.com>
2021-08-18 21:32:16 +0900

    37th_commit_splash

---
+++ app/src/main/java/org/techtown/oasis/SplashActivity.java
@@ -0,0 +1,24 @@
+package org.techtown.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 1000);  // 1초 후 화면 전환
+    }
+}
